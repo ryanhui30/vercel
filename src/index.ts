5cd0c44a2cd4619eeff2052f2c627f94794b667c
Ryan Hui <ryanhui30@gmail.com>
2025-08-13 15:19:01 -0700

    add server file logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import express from "express";
 import cors from "cors";
 import { simpleGit } from "simple-git";
 import { generate } from "./utils.js";
+import { getAllFiles } from "./files.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Get the equivalent of __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors())
@@ -10,10 +17,13 @@ app.use(express.json());
 // POSTMAN
 app.post("/deploy", async (req, res) => {
     const repoUrl = req.body.repoUrl;
-    console.log(repoUrl);
     const id = generate();
-    await simpleGit().clone(repoUrl, `output/${id}`);
-      res.json({
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
+    console.log("Files:", files); // Add this line
+
+    res.json({
         id: id
     })
 });
